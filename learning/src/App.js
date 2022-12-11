0d53bbe8ef87bb3cde34e0e029c75efeadb58239
Drumil_Akhenia <akheniad@gmail.com>
2022-12-11 18:28:11 +0530

    added props learning
    we can set default values of props and we can also specify fix data type of props

---
+++ learning/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header bgColor="red" textColor="green" text={123}/>
+        <div>
+          My App
+        </div>
+    </>
+  );
+}
+
+export default App;
---
+++ learning/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const Header = ({bgColor,textColor,text})=>{
+    const style = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+    return(
+        <>
+            <p style={style}>
+                {text}
+            </p>
+        </>
+    )
+}
+
+Header.defaultProps = {
+    bgColor:'green',
+    textColor:'black',
+}
+
+Header.propTypes = {
+    text : PropTypes.string.isRequired,
+    bgColor : PropTypes.string,
+    textColor : PropTypes.string
+}
+
+export default Header;
